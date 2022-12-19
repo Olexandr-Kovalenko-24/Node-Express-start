4ca38f4b2e9b6bedd3cf4c06ec147f0763ea980e
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-12-19 14:34:07 +0200

    express install

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const express = require('express');
+const app = express();
+const http = require('http');
 
+const PORT = 3000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, ()=>{
+    console.log(`App is started on port ${PORT}`)
+});
